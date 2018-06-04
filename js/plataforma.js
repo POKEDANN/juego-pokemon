c6ba8dbab51a33afc7fc64d2281a7667179756a8
Daniel Cruz <Pokedann@MacBook-Pro-de-Daniel.local>
2018-06-04 01:24:01 -0500

    Mi version
    Aqui esta la plataforma, se pusieron 2 pokemon desde los sprites de una
    pagina con su barra de puntos de vida, cada uno tiene su select con su
    respectivo boton de atacar, he estado intentando que al hacer click en
    el boton la barra de progreso disminuya segun el ataque elegido en el
    select con jquery y su funcion .click pero por alguna razon no logro
    hacerlo

--- js/plataforma.js
+++ js/plataforma.js
@@ -1,7 +1,11 @@
+var ataq = $( "#ataquesE option:selected" ).val();
+var ataq2 = $( "#ataquesE option:selected" ).text();
+console.log(ataq);
+console.log(ataq2);
+
 $(document).ready(function () {
-	$("#atacar").on("click",function() {
-		// $(".progress-bar").css("width", "75%");
-		if("#atacar" == "thunder punch"){
+	$("body").on("click",'#atacar',function() {
+		if(ataq == 'thunder punch'){
 				$(".progress-bar").width("75%");
 			}
 		});

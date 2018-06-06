398507dc6731a69b346539a04eba6ee47878c0bc
Daniel Cruz <Pokedann@MacBook-Pro-de-Daniel.local>
2018-06-05 22:35:33 -0500

    atacar funcionando
    Gracias a la ayuda de un compañero lo que queria ya funciona, al
    apretar el boton atacar se modifica el width de la progress bar del
    contrario como si le hiciera daño, todo por medio de js, se dieron
    algunos estilos a la plataforma

--- js/plataforma.js
+++ js/plataforma.js
@@ -1,12 +1,31 @@
-var ataq = $( "#ataquesE option:selected" ).val();
-var ataq2 = $( "#ataquesE option:selected" ).text();
-console.log(ataq);
-console.log(ataq2);
+$(document).ready(function() {
+  $('.pokemon-player').on('submit', 'form', function(event) {
+    event.preventDefault();//cuando un boton esta adentro de un form este
+    // event es para para evitar que se recargue la pagina
 
-$(document).ready(function () {
-	$("body").on("click",'#atacar',function() {
-		if(ataq == 'thunder punch'){
-				$(".progress-bar").width("75%");
-			}
-		});
-	});
+    var $pokemonPlayer = $(this).closest('section');//busca el pariente o padre mas cercano
+
+    var $oponent = $($pokemonPlayer.data('oponent'));//aqui busca los datas que pusimos
+    // en este caso es el de oponent, a los datas se les pone "data"-cualquier nombre
+
+    var $progressBar = $oponent.find('.barEle,.barLuca');//esto es para buscar una clase
+    var attack = $pokemonPlayer.find('select').val();//aqui busca el valor seleccionado en
+    // el select
+
+    var rand = Math.floor(Math.random() * 15) + 1;//aqui selecciona un numero random hasta
+    // el 15
+
+    var width = $progressBar.width() / $progressBar.parent().width() * 100;//aqui le da
+    // width a la progress-bar en porcentaje %
+    $("#vidaEle,#vidaLuca").prepend(width);
+
+    var new_width = width - rand;//aqui le da el nuevo width
+
+    if(new_width > 0) {
+      $progressBar.css('width', new_width+'%');//si es mayor a 0 le da el nuevo width
+    } else {
+      $progressBar.css('width', 0+'%');//si no ya no le da width
+      alert('Ganador ' + $pokemonPlayer.data('oponent'));//manda el alert diciendo el ganador
+    }
+  });
+});
